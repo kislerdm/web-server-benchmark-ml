b8dad459ba4e81de38ac300f4eba003f7ab8387b
Dmitry Kisler <admin@dkisler.de>
2019-08-29 19:44:06 +0200

    add trained model@0.9832 acc

--- mnist/model.py
+++ mnist/model.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.datasets import mnist
 import numpy as np
+import time
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -13,7 +14,7 @@ np.random.seed(2019)
 PATH_OUT = os.path.join(os.getcwd(), "mnist_model_py.tf")
 
 LEARNING_RATE = 1e-3
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 LIMIT_EPOCHS = 10000
 
 
@@ -31,9 +32,11 @@ def model_definition() -> keras.Sequential:
     """ Function to define the model """
 
     return keras.Sequential([
-        keras.layers.Flatten(input_shape=(28, 28)),
-        keras.layers.Dense(128, activation='relu'),
-        keras.layers.Dense(10, activation='softmax')
+        keras.layers.Flatten(input_shape=(28, 28), name="l_0"),
+        keras.layers.Dense(200, activation='relu', 
+                           kernel_regularizer=keras.regularizers.l2(.001), 
+                           name="l_1"),
+        keras.layers.Dense(10, activation='softmax', name="l_out")
     ])
 
 
@@ -49,11 +52,10 @@ def callbacks_definition(learning_rate: float = .001) -> list:
         return learning_rate * np.exp(alpha * (epoch_thresh - epoch))
 
     return [keras.callbacks.EarlyStopping(monitor='val_loss',
-                                          min_delta=1e-3,
+                                          min_delta=1e-4,
                                           patience=2,
                                           verbose=0,
                                           mode='min',
-                                          baseline=None,
                                           restore_best_weights=True),
             keras.callbacks.LearningRateScheduler(_scheduler),
             ]
@@ -61,11 +63,15 @@ def callbacks_definition(learning_rate: float = .001) -> list:
 
 if __name__ == "__main__":
     try:
+        # init session
+        session = tf.compat.v1.Session()
+        tf.compat.v1.keras.backend.set_session(session)
         model = model_definition()
 
         model.compile(optimizer=keras.optimizers.Adam(learning_rate=LEARNING_RATE),
                       loss="sparse_categorical_crossentropy",
                       metrics=["accuracy"])
+        
     except Exception as ex:
         print("Model definition and compilation problem. Error:\n%s", ex)
         sys.exit(1)
@@ -87,8 +93,14 @@ if __name__ == "__main__":
               callbacks=callbacks)
 
     evaluation = model.evaluate(x_test, y_test, verbose=0)
-
     print("Model test evaluation: {}% accuracy.".format(round(evaluation[1] * 100, 2)))
-
+    
     print(f"Saving model to {PATH_OUT}")
-    model.save(PATH_OUT)
+    if os.path.isdir(PATH_OUT):
+        os.system(f"mv -f {PATH_OUT} {PATH_OUT}_dump_{time.strftime('%Y%m%d%H%M%S')}")
+        
+    builder = tf.compat.v1.saved_model.builder.SavedModelBuilder(PATH_OUT)
+    builder.add_meta_graph_and_variables(session, ["model_py"])
+    builder.save()    
+    
+    session.close()
