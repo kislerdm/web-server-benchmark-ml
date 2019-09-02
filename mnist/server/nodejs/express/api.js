0152984762539f339d1044cdec5751d8b3e92446
Dmitry Kisler <admin@dkisler.de>
2019-09-02 07:55:32 +0200

    fix bracket

--- mnist/server/nodejs/express/api.js
+++ mnist/server/nodejs/express/api.js
@@ -56,7 +56,7 @@ app.get('/', (req, res) => {
               digit: prediction.indexOf(prob),
               probability: prob,
             }
-          }));
+          });
         });
       };
     });
